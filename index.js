8a7517c29c641884cd100a643c98d877a94661e1
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-07 14:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // Add your functions here
-function map(arr) {
-  const newArr = arr.map(function(index) {
-    return index * -1;
-  })
-  return newArr;
+function map(obj, fn) {
+  const arr = []
+  for (const key in obj) {
+    arr.push(fn(obj[key]))
+  }
+  return arr
+}
+
+function reduce(obj, cb, accumulator) {
+  const keys = Object.keys(obj)
+  let i = 1
+
+  if (accumulator) {
+    i = 0
+  } else {
+    accumulator = obj[keys[0]]
+  }
+
+  for (i; i < keys.length; i++) {
+    accumulator = cb(accumulator, obj[keys[i]])
+  }
+  return accumulator
 }
